1d36ebb2e8934ffb15f418a2ce8b450753788dba
Mathias Brodala <mbrodala@pagemachine.de>
2026-03-19 11:03:29 +0100

    [TASK] Drop TYPO3 CMS project comment
    This project is not part of TYPO3.

--- Tests/Functional/ConcerningFileReferences.php
+++ Tests/Functional/ConcerningFileReferences.php
@@ -9,18 +9,6 @@
 
 namespace Pagemachine\FileVariants\Tests\Functional;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
 use PHPUnit\Framework\Attributes\Test;
 use TYPO3\CMS\Backend\Controller\File\FileController;
 use TYPO3\CMS\Core\Core\Environment;
