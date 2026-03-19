6dde7f44b4f13b439ef0a1f7ae5f8ab28cc20f93
Mathias Brodala <mbrodala@pagemachine.de>
2026-03-19 10:03:33 +0100

    [TASK] Drop PHP header comment requirement

--- Build/.php-cs-fixer.dist.php
+++ Build/.php-cs-fixer.dist.php
@@ -1,19 +1,9 @@
 <?php
 
-$header = <<<EOF
-This file is part of the package t3g/file_variants.
-
-For the full copyright and license information, please read the
-LICENSE file that was distributed with this source code.
-EOF;
-
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config
     ->setCacheFile(__DIR__ . '/.php-cs-fixer.cache')
     ->setRules([
-        'header_comment' => [
-            'header' => $header
-        ],
         'general_phpdoc_annotation_remove' => [
             'annotations' => [
                 'author',
